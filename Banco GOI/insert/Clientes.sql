4067c455218180964b8f07c5c4ac580b4c57cd32
Gusta <aphexragedev@gmail.com>
2025-05-16 17:46:55 -0300

    Fix: Nome do Cliente Gustavo

--- Banco GOI/insert/Clientes.sql
+++ Banco GOI/insert/Clientes.sql
@@ -1,2 +1,6 @@
 INSERT INTO Cliente (Nome, Sobrenome, CPF, Telefone, CEP, Data_Nascimento, Email)
 VALUES ('Gustavo', 'Mendes dos Santos', '5075263818', '11987654321', '01167219', '2005-01-01', 'gustavo@gmail.com');
+
+UPDATE Cliente
+SET Sobrenome = 'Mendes'
+WHERE Nome = 'Gustavo' AND id = 9;
