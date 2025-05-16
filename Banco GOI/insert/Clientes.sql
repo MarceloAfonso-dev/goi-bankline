3f39031ac8c3e508c089d16310738a161b9f7e6c
Gusta <aphexragedev@gmail.com>
2025-05-16 17:49:51 -0300

    Update Clientes.sql

--- Banco GOI/insert/Clientes.sql
+++ Banco GOI/insert/Clientes.sql
@@ -1,6 +1,8 @@
+-- Insert Query com meu nome e dados ficticios
 INSERT INTO Cliente (Nome, Sobrenome, CPF, Telefone, CEP, Data_Nascimento, Email)
 VALUES ('Gustavo', 'Mendes dos Santos', '5075263818', '11987654321', '01167219', '2005-01-01', 'gustavo@gmail.com');
 
+-- Update Query
 UPDATE Cliente
 SET Sobrenome = 'Mendes'
 WHERE Nome = 'Gustavo' AND id = 9;
