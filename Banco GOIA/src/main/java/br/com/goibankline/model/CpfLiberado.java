8140b515d6f2c2732cd769c59399571d4a182dc7
Marcelo Afonso <dev.marceloafonso@gmail.com>
2025-10-11 20:56:02 -0300

    fix: nova logo e ajuste em header

---
+++ Banco GOIA/src/main/java/br/com/goibankline/model/CpfLiberado.java
@@ -0,0 +1,17 @@
+package br.com.goibankline.model;
+
+public class CpfLiberado {
+    private String cpf;
+
+    public CpfLiberado(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
